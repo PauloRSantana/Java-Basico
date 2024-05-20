0018cce0f1e1529675700739936566e0979d2261
Paulo Ricardo <pauloricardo.network@gmail.com>
2024-05-20 13:59:57 -0300

    Subindo código
    !

--- Do While/src/App.java
+++ Do While/src/App.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class App {
+
+
     public static void main(String[] args) {
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
 
         System.out.println("Digite a temperatura em Celsius: ");
         double C = sc.nextDouble();
@@ -12,5 +16,7 @@ public class App {
         System.out.printf("Equivalente em Fahrenheit: %.2f%n", F);
 
         sc.close();
+
+
     }
 }
